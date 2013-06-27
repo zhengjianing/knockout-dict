5534f55792fb24726cd030b83d4720081ffcff47
jianing <twer@TWers-MacBook-Pro-5.local>
2013-06-27 17:42:13 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnockoutDict::Application.config.secret_key_base = 'c5e92c33852bcdf53ec47fbfcfdf8b1c0c295ab07a604ce5ccbe6388ac13bad4405f70907249d9a53460da20e9453749acd2496d7ccac38308279df862c19387'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KnockoutDict::Application.config.session_store :cookie_store, key: '_knockout-dict_session'
